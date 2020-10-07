bdc762acc423dfd1f5fcabe523ed3f0597d4e111
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2020-10-07 09:12:52 +0700

    commit lagi

--- app/src/main/java/com/example/appnumber3/MainActivity.java
+++ app/src/main/java/com/example/appnumber3/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,35 +22,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        TextView txt1 = findViewById(R.id.txt1);
+        TextView txt2 = findViewById(R.id.txt2);
+        TextView txt3 = findViewById(R.id.txt3);
+        TextView txt4 = findViewById(R.id.txt4);
+        TextView txt5 = findViewById(R.id.txt5);
+
+        String myStringValue = "Android Programming is FUN!";
+
+        txt1.setText(myStringValue);
+
+        txt2.setText(myStringValue.charAt(0) + "");
+        //to convert return type of charAt  we need to concatinate
+            // with "" becuse it need a string
+        txt3.setText(myStringValue.substring(2)); //start at no ending
+        txt4.setText(myStringValue.substring(5, 9)); //start at ending at(ending eliminated)
+//        ini commit github
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
